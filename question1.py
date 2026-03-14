ff647b3a32f6779c5761355768a0255000f86394
Mastewal Tesfaye <153563753+mastewalt@users.noreply.github.com>
2026-03-14 11:46:52 +0100

    add docstring to snack shop program describing program requirements

--- question1.py
+++ question1.py
@@ -1,3 +1,12 @@
+'''
+1)	Build a program that:
+•	Displays a list of snacks and drinks with item numbers and prices. 
+•	Ask the user to choose items by number in a loop.
+•	 Keeps track of selected items and their prices.
+•	Ends when the user types "done".
+•	Finally prints a receipt showing: List of selected items with prices and total cost
+'''
+
 # 1. Menu dictionary
 menu = {
     1: {"name": "Chips", "price": 4.50},
